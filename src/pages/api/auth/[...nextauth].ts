62bb1f7bd2aed40ed663c2fb06a5be248b2eac2f
Horus Hong <horus@192-168-1-105.tpgi.com.au>
2023-03-14 22:59:24 +1100

    Initial authentication with Google provider

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,61 @@
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from '../../../lib/prismadb'
+import { redirect } from 'next/dist/server/api-utils'
+
+const getGoogleCredentials = () => {
+  const clientId = process.env.GOOGLE_CLIENT_ID
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET
+
+  if (!clientId || clientId.length === 0) {
+    throw new Error('No clientId for google provider.')
+  }
+
+  if (!clientSecret || clientSecret.length === 0) {
+    throw new Error('No clientSecret for google provider.')
+  }
+
+  return { clientId, clientSecret }
+}
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: getGoogleCredentials().clientId,
+      clientSecret: getGoogleCredentials().clientSecret,
+    }),
+  ],
+  session: {
+    strategy: 'jwt'
+  },
+  events: {
+    async signIn(message) {
+      console.log('Signed in with: ')
+      console.log(message)
+    },
+    async signOut(message) {
+      console.log('Signed out: ')
+      console.log(message)
+    },
+  },
+  callbacks: {
+    async session({ token, session }) {
+      if (token) { 
+        session.user.id = token.id
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.picture
+      }
+
+      return session
+    },
+    async jwt({ token, user }) {
+
+      return token
+    },
+  },
+}
+
+export default NextAuth(authOptions)
