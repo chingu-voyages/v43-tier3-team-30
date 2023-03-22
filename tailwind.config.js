8e99714137a350b1311ef4df05a01316a93ec1b0
Devansh-365 <devanshtiwari365@gmail.com>
2023-03-22 08:54:02 +0530

    fix: remove unwanted code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './next.config.js'],
-  variants: {
-    backgroundColor: ['children', 'DEFAULT', 'hover', 'focus'],
-  },
   theme: {
     extend: {},
   },
