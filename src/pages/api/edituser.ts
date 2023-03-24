bbb01158491c4f78ba01040b158d73f23dfb3f94
Devansh-365 <devanshtiwari365@gmail.com>
2023-03-24 22:36:05 +0530

    feat(api): add edituser endpoint

---
+++ src/pages/api/edituser.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import serverAuth from '@/lib/serverAuth'
+import db from '@/lib/prismadb'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'PATCH') {
+    return res.status(405).end()
+  }
+
+  try {
+    const { currentUser } = await serverAuth(req)
+
+    console.log(currentUser)
+
+    const { name, image } = req.body
+
+    if (!name) {
+      throw new Error('Missing fields')
+    }
+
+    const updatedUser = await db.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        name,
+        image
+      },
+    })
+
+    return res.status(200).json(updatedUser)
+  } catch (error) {
+    console.log(error)
+    return res.status(400).end()
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,21 @@
 import { Button, buttonVariants } from '@/components/Button'
+import Input from '@/components/Input'
 import useCurrentUser from '@/hooks/useCurrentUser'
 import { useToast } from '@/hooks/useToast'
+import { Avatar, AvatarImage, AvatarFallback } from '@radix-ui/react-avatar'
+import axios from 'axios'
 import type { NextPage } from 'next'
 import { signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useCallback, useState } from 'react'
 
 const Home: NextPage = () => {
   const { data: session, status } = useSession()
   const { data: currentUser } = useCurrentUser()
+
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [name, setName] = useState<string>(currentUser?.name)
 
   const { toast } = useToast()
 
@@ -16,11 +23,45 @@ const Home: NextPage = () => {
 
   console.log(status === 'authenticated' && session.user)
 
+  const onSubmit = useCallback(async () => {
+    try {
+      setIsLoading(true)
+
+      await axios.patch('/api/edituser', {
+        name
+      })
+
+      toast({
+        variant: 'success',
+        description: 'Name updated',
+      })
+
+    } catch (error) {
+      toast({
+        variant: 'destructive',
+        description: 'Something went wrong',
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }, [
+    name
+  ])
+
   if (status === 'authenticated') {
     return (
-      <div className="flex min-h-screen flex-col bg-[url('/noise.png')] items-center justify-center py-2 bg-[#18191b] text-white">
-        {currentUser?.image != null && (
-          <Image src={currentUser?.image} width={10} height={10} alt="" />
+      <div className="flex text-center space-y-4 min-h-screen flex-col bg-[url('/noise.png')] items-center justify-center py-2 bg-[#18191b] text-white">
+        {currentUser && (
+          <Avatar className="uppercase rounded-full">
+            <AvatarImage
+              className="rounded-full h-10"
+              src={currentUser?.image}
+              alt=""
+            />
+            <AvatarFallback className="bg-[#16181A] px-3 py-2 border-gray-400 border rounded-full">
+              {currentUser?.name.match(/\b(\w)/g).join(' ')}
+            </AvatarFallback>
+          </Avatar>
         )}
         <p>
           Signed in as {currentUser?.email} & {currentUser?.name}
@@ -38,6 +79,14 @@ const Home: NextPage = () => {
           }
         >
           Click me
+        </Button>
+        <Input
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <Button onClick={onSubmit} className="my-2" >
+          Edit Name
         </Button>
       </div>
     )
