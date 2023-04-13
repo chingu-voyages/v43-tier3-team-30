b2bf6979cd072ca9d950f1c673dcef9ca99b2f05
Devansh-365 <devanshtiwari365@gmail.com>
2023-04-13 19:22:09 +0530

    feat: update event modal

--- src/components/EventModal.tsx
+++ src/components/EventModal.tsx
@@ -9,93 +9,245 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog'
 import Input from '@/components/ui/Input'
-
-import { useCallback, useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import { useToast } from '@/hooks/useToast'
 import axios from 'axios'
 import useEvents from '@/hooks/useEvents'
+import { Typography } from './ui/Typography'
+import ImageUpload from './ui/ImageUpload'
+import { useRouter } from 'next/router'
+import { Textarea } from './ui/textarea'
+import TagsInput, { tags } from './TagsInput'
 
-type Values = {
+interface HeadingProps {
   title: string
-  note: string
+  subtitle?: string
 }
 
-export function EventModal() {
+enum STEPS {
+  PUBLIC = 0,
+  INFO = 1,
+  IMAGES = 2,
+  TAG = 3,
+  LOCATION = 4,
+}
+
+const Heading: React.FC<HeadingProps> = ({ title, subtitle }) => {
+  return (
+    <div>
+      <Typography variant="h5">{title}</Typography>
+      <Typography variant="bodytext1" className="mt-2 text-gray-600">
+        {subtitle}
+      </Typography>
+    </div>
+  )
+}
+
+export function EventModal({ children }: any) {
   const { toast } = useToast()
+  const router = useRouter()
 
-  const [values, setValues] = useState<Values>({
-    title: '',
-    note: '',
-  })
   const [isLoading, setIsLoading] = useState(false)
+  const [values, setValues] = useState({
+    title: '',
+    brochure_img: '',
+    isPublic: true,
+    tag: '',
+    note: '',
+    location: '',
+  })
 
-  const { mutate: mutateEvents } = useEvents()
-
-  console.log('event : ', values)
-
-  const handleSubmit = useCallback(async () => {
-    try {
-      setIsLoading(true)
-      console.log(values.title)
-
-      await axios.post('/api/events', values)
-
-      setIsLoading(false)
-
-      toast({
-        variant: 'success',
-        description: 'Event created',
-      })
-
-      mutateEvents()
-      setValues({
-        title: '',
-        note: '',
-      })
-    } catch (error) {
-      toast({
-        variant: 'destructive',
-        description: 'Something went wrong',
-      })
-    } finally {
-      setIsLoading(false)
-    }
-  }, [values, mutateEvents])
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
     setValues({ ...values, [event.target.name]: event.target.value })
+  }
+
+  console.log(values)
+
+  const [step, setStep] = useState(STEPS.PUBLIC)
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.LOCATION) {
+      return 'Create'
+    }
+
+    return 'Next'
+  }, [step])
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.PUBLIC) {
+      return undefined
+    }
+
+    return 'Back'
+  }, [step])
+
+  const onBack = () => {
+    setStep((value) => value - 1)
+  }
+
+  const onNext = () => {
+    setStep((value) => value + 1)
+  }
+
+  let content = (
+    <div className="flex flex-col gap-8">
+      <Heading title="Wanted to keep event private?" subtitle="Yes or no" />
+      <ul
+        className="
+          grid 
+          grid-cols-1 
+          md:grid-cols-2 
+          gap-4
+          max-h-[50vh]
+          overflow-y-auto
+        "
+      >
+        <li
+          onClick={() => setValues({ ...values, isPublic: false })}
+          className={`rounded-xl border-2 ${
+            values.isPublic
+              ? 'border-neutral-200 dark:border-neutral-600'
+              : 'border-black dark:border-white'
+          } p-4 flex flex-col gap-3 hover:border-black dark:hover:border-white transition cursor-pointer`}
+        >
+          Yes, keep it private
+        </li>
+        <li
+          onClick={() => setValues({ ...values, isPublic: true })}
+          className={`rounded-xl border-2 ${
+            !values.isPublic
+              ? 'border-neutral-200 dark:border-neutral-600'
+              : 'border-black dark:border-white'
+          } p-4 flex flex-col gap-3 hover:border-black dark:hover:border-white transition cursor-pointer`}
+        >
+          No, shared with all
+        </li>
+      </ul>
+    </div>
+  )
+
+  if (step === STEPS.TAG) {
+    content = (
+      <div className="flex flex-col gap-8">
+        <Heading title="Describe event in one word?" subtitle="Pick your tag" />
+        <div
+          className="
+          grid 
+          grid-cols-1 
+          md:grid-cols-2 
+          gap-3
+          max-h-[50vh]
+          overflow-y-auto
+        "
+        >
+          {tags.map((item) => (
+            <div key={item.label} className="col-span-1">
+              <TagsInput
+                onClick={(tag) => setValues({ ...values, tag: tag })}
+                selected={values.tag === item.label}
+                label={item.label}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+
+  if (step === STEPS.INFO) {
+    content = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Tell us some info about event."
+          subtitle="Give us info related to event"
+        />
+        <Input
+          id="title"
+          name="title"
+          label="Title"
+          onChange={handleChange}
+          disabled={isLoading}
+          required
+        />
+        <Textarea
+          id="note"
+          name="note"
+          label="Note"
+          onChange={handleChange}
+          disabled={isLoading}
+          required
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.IMAGES) {
+    content = (
+      <div className="flex flex-col gap-8">
+        <Heading title="Add a photo of your event" subtitle="Upload images" />
+        <ImageUpload
+          onChange={(e) => setValues({ ...values, brochure_img: e })}
+          value={values.brochure_img}
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.LOCATION) {
+    content = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Where did the event occur?"
+          subtitle="Give us location of event"
+        />
+        <Textarea
+          id="location"
+          name="location"
+          label="Location"
+          onChange={handleChange}
+          disabled={isLoading}
+          required
+        />
+      </div>
+    )
   }
 
   return (
     <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">Create Event</Button>
-      </DialogTrigger>
+      <DialogTrigger asChild>{children}</DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Create Event</DialogTitle>
-          <DialogDescription>
+        <DialogHeader className="mb-4">
+          <DialogTitle className="text-start">Create Event</DialogTitle>
+          {/* <DialogDescription>
             Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
+          </DialogDescription> */}
+          <hr className="text-gray-600 dark:text-gray-500 h-[1px]" />
         </DialogHeader>
-        <Input
-          id="title"
-          name="title"
-          placeholder="What's the event name?"
-          className="col-span-3"
-          onChange={handleChange}
-        />
-        <Input
-          id="note"
-          name="note"
-          placeholder="Have some notes?"
-          className="col-span-3"
-          onChange={handleChange}
-        />
+        <div className="flex flex-col h-[50vh] md:h-auto">{content}</div>
         <DialogFooter>
-          <Button disabled={isLoading} onClick={handleSubmit} type="submit">
-            Submit
-          </Button>
+          <div className="flex gap-2 md:gap-4 md:mt-4">
+            {secondaryActionLabel && (
+              <Button
+                disabled={isLoading}
+                onClick={onBack}
+                variant="outline"
+                className="w-full"
+              >
+                {secondaryActionLabel}
+              </Button>
+            )}
+            {actionLabel !== 'Next' ? (
+              <Button disabled={isLoading} className="w-full">
+                Create
+              </Button>
+            ) : (
+              <Button disabled={isLoading} onClick={onNext} className="w-full">
+                {actionLabel}
+              </Button>
+            )}
+          </div>
         </DialogFooter>
       </DialogContent>
     </Dialog>
